6bd8b43c10774f649a6126460ccd4c0774f29a9c
kkm4512 <nayoun440@naver.com>
2024-08-09 17:46:49 +0900

    crud create 1. HelloController.java / crud create

--- src/main/java/com/sparta/springprepare/controller/HelloController.java
+++ src/main/java/com/sparta/springprepare/controller/HelloController.java
@@ -1,13 +1,37 @@
 package com.sparta.springprepare.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
-    @GetMapping("/api/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/get")
+    @ResponseBody
+    public String get() {
+        return "Get Method 요청 !";
+    }
+
+    @PostMapping("/post")
+    @ResponseBody
+    public String post() {
+        return "post Method !!!";
+    }
+
+    @PutMapping("/put")
+    @ResponseBody
+    public String put() {
+        return "PUT 요청 !!!";
+    }
+
+    @DeleteMapping("/delete")
+    @ResponseBody
+    public String delete() {
+        return "deleted !!!";
     }
 }
