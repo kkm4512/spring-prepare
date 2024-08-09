be48eac0e49f85036fff37e274935e650cd488dd
kkm4512 <nayoun440@naver.com>
2024-08-09 17:37:49 +0900

    tymleaf gradle ìadd

--- src/main/java/com/sparta/springprepare/controller/HelloController.java
+++ src/main/java/com/sparta/springprepare/controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.sparta.springprepare.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/api/hello")
+    @ResponseBody
     public String hello() {
-        return "Hello World!";
+        return "Hello World!!!";
     }
+
+
 }
