a526e26ff4d8debab1253603612ca5a9a84269d3
kkm4512 <nayoun440@naver.com>
2024-08-11 21:00:47 +0900

    update-delete feature add

--- src/main/java/com/sparta/springprepare/controller/MemoController.java
+++ src/main/java/com/sparta/springprepare/controller/MemoController.java
@@ -37,4 +37,29 @@ public class MemoController {
         // Map To List
         return memoList.values().stream().map(MemoResponseDto::new).toList();
     }
+
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        // 해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            //일단 그냥 entity 그자체로 가져옴
+            Memo memo = memoList.get(id);
+
+            // memo 수정
+            memo.update(requestDto);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        if (memoList.containsKey(id)) {
+            memoList.remove(id);
+        } else {
+            throw new IllegalArgumentException("해당 메모는 존재하지 않습니다 !");
+        }
+        return id;
+    }
 }
